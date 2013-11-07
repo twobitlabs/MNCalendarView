1db302aa9176d6cd79ed5c5f481181f6a58fdae6
Tibor Bödecs <mail.tib@gmail.com>
2013-11-07 10:20:33 +0100

    ios6 crash fix

--- MNCalendarView/MNCalendarViewLayout.m
+++ MNCalendarView/MNCalendarViewLayout.m
@@ -10,51 +10,56 @@
 
 @implementation MNCalendarViewLayout
 
-- (id)init {
-  if (self = [super init]) {
-    self.sectionInset = UIEdgeInsetsZero;
-    self.minimumInteritemSpacing = 0.f;
-    self.minimumLineSpacing = 0.f;
-    self.headerReferenceSize = CGSizeMake(0.f, 44.f);
-    self.footerReferenceSize = CGSizeZero;
-  }
-  return self;
+- (id)init
+{
+    if (self = [super init]) {
+        self.sectionInset   = UIEdgeInsetsZero;
+        self.minimumInteritemSpacing = 0.f;
+        self.minimumLineSpacing = 0.f;
+        self.headerReferenceSize = CGSizeMake(0.f, 44.f);
+        self.footerReferenceSize = CGSizeZero;
+    }
+    return self;
 }
 
-- (CGPoint)targetContentOffsetForProposedContentOffset:(CGPoint)proposedContentOffset withScrollingVelocity:(CGPoint)velocity {
-  
+- (CGPoint)targetContentOffsetForProposedContentOffset:(CGPoint)proposedContentOffset withScrollingVelocity:(CGPoint)velocity
+{
     if (!_pagingEnable) {
         return proposedContentOffset;
     }
     
-  NSArray *array =
-    [super layoutAttributesForElementsInRect:({
-      CGRect bounds = self.collectionView.bounds;
-      bounds.origin.y = proposedContentOffset.y - self.collectionView.bounds.size.height/2.f;
-      bounds.size.width *= 1.5f;
-      bounds;
+    NSArray *array = [super layoutAttributesForElementsInRect:({
+        CGRect bounds = self.collectionView.bounds;
+        bounds.origin.y = proposedContentOffset.y - self.collectionView.bounds.size.height/2.f;
+        bounds.size.width *= 1.5f;
+        bounds;
     })];
-  
-  CGFloat minOffsetY = CGFLOAT_MAX;
-  UICollectionViewLayoutAttributes *targetLayoutAttributes = nil;
+    
+    CGFloat minOffsetY = CGFLOAT_MAX;
+    UICollectionViewLayoutAttributes *targetLayoutAttributes = nil;
+    
+    for (UICollectionViewLayoutAttributes *layoutAttributes in array) {
+        if ([layoutAttributes.representedElementKind isEqualToString:UICollectionElementKindSectionHeader]) {
+            CGFloat offsetY = fabs(layoutAttributes.frame.origin.y - proposedContentOffset.y);
+            
+            if (offsetY < minOffsetY) {
+                minOffsetY = offsetY;
 
-  for (UICollectionViewLayoutAttributes *layoutAttributes in array) {
-    if ([layoutAttributes.representedElementKind isEqualToString:UICollectionElementKindSectionHeader]) {
-      CGFloat offsetY = fabs(layoutAttributes.frame.origin.y - proposedContentOffset.y);
-
-      if (offsetY < minOffsetY) {
-        minOffsetY = offsetY;
-
-        targetLayoutAttributes = layoutAttributes;
-      }
+                targetLayoutAttributes = layoutAttributes;
+            }
+        }
     }
-  }
+    
+    if (targetLayoutAttributes) {
+        return targetLayoutAttributes.frame.origin;
+    }
+    
+    return CGPointMake(proposedContentOffset.x, proposedContentOffset.y);
+}
 
-  if (targetLayoutAttributes) {
-    return targetLayoutAttributes.frame.origin;
-  }
-
-  return CGPointMake(proposedContentOffset.x, proposedContentOffset.y);
+- (UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return [UICollectionViewLayoutAttributes layoutAttributesForCellWithIndexPath:indexPath];
 }
 
 @end
